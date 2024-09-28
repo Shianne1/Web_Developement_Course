14359069f37b158091f36549d7e906400044443f
Shianne <slesure1@ggc.edu>
2024-09-27 21:07:53 -0400

    Update index.php and Menu.php
    everything is working except for the chocolate cake image. I am not sure why it is big.

--- chp-03/Ch3_Bakery/index.php
+++ chp-03/Ch3_Bakery/index.php
@@ -1,10 +1,13 @@
 <?php 
 $productsArray = [
-    0=> ['Apple Pie', 12.51], 
-    1=> ['Cheesecake', 5.5],
+    0 => ['Apple Pie', 12.5], 
+    1 => ['Cheesecake', 5.50],
     2 => ['Croissant', 2.5],
     3 => ['Doughnut', 0.5],
-    4 => ['Pastel de Nata', 4]]?>
+    4 => ['Pastel de Nata', 4]];
+
+    $GLOBALS['productsArray'] = $productsArray;
+    ?>
 
 
 <!DOCTYPE html>
@@ -12,7 +15,7 @@ $productsArray = [
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
+    <title>Clementine Bakery-Home</title>
     <link rel = "stylesheet" href = "https://www.w3schools.com/w3css/4/w3.css">
     <!-- Gooogle fonts -->
      <link rel = "stylesheet" href = "https://fonts.googleapis.com/css?family=Pacifico"> <!-- a GET request -->
@@ -25,46 +28,43 @@ $productsArray = [
     <div class="w3-container w3-center w3-pink">
         <h1>Clementine Bakery</h1>
         <h2>Order Form</h2>
-        <img src="ChocolateCake.png" width = "10%" height = "10%" class="w3-display-topleft">
-        <img src="Croissant_PNG_Clip_Art-2216.png"" width = "10%" height = "10%" class="w3-display-topright">
+        <img src="ChocolateCake.png" width = "10%" class="w3-display-topleft">
+        <img src="Croissant_PNG_Clip_Art-2216.png" width = "10%" height = "10%" class="w3-display-topright">
     </div>
     <?phP include "menu.php"; ?>
     <form action = "ProcessOrder.php" mthod = "POST">
         <label>Select Product</label>
-        <select> name = "product" class = "w3-select">
+        <select name = "product" class = "w3-select">
             <?php 
+            // loop through the $product Array
             foreach($productsArray as $key =>$value)
-            echo "<option value = '$key'> $value[0] </option>";
+            echo "<option value = '$key'> $value[0] </option><br>";
         ?>
         </select>
 
-        <label>Quantity</label>
-        <input type="text" class="w3-input" name = "quantity" size = "3">
+        <label>Enter quantity</label>
+        <input type="text" class="w3-input" name = "quantity" size = "4" maxlength = "4">
 
-        <input type = "submit" value = "Submit order">
+        <input type = "submit" name = "submit" value = "Submit Order">
 
     </form>
-
     <div class="w3-container">
         <table class="w3-table w3-table-all">
-            <tr class="w3-blue-grey">
+            <tr class="w3-blue-gray">
                 <th>Item code</th>
                 <th>Description</th>
                 <th>Unit Price</th>
             </tr>
             <?php 
-            foreach($productsArray as $key =>$value){
-            echo "<tr>";
-            echo "<td>$key</td>";
-            echo "<td>$value[0]</td>";
-            echo "<td>".number_format($value[1], 2). "</td>";
-            echo "</tr>";
-            }
+                foreach($productsArray as $key =>$value){
+                echo "<tr>";
+                echo "<td>$key</td>";
+                echo "<td>$value[0]</td>";
+                echo "<td> $".number_format($value[1], 2). "</td>";
+                echo "</tr>";
+                }
             ?>
         </table>
-    </div>
-
-
-    
+    </div>   
 </body>
 </html>
