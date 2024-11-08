65c3b9de19006a00d00d67f352777a4d57a5ed98
Shianne <slesure1@ggc.edu>
2024-11-08 16:17:15 -0500

    now showing orders

--- chp-8-11/BookStoreDB/showBooks.php
+++ chp-8-11/BookStoreDB/showBooks.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>BookStore - View Customers</title>
+    <title>BookStore - View Books</title>
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <link rel="stylesheet" href='styles.css'>
 </head>
---
+++ chp-8-11/BookStoreDB/showOrders.php
@@ -0,0 +1,69 @@
+<?php include "utilFunctions.php"; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BookStore - View Customers</title>
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href='styles.css'>
+</head>
+<body>
+    <div class="w3-container w3-blue-grey">
+        <header class="w3-container w3-center">
+            <h1>Bookstore</h1>
+            <h2>View All Books</h2>
+        </header>
+        <?php include 'mainMenu.php' ?>
+
+    <div class="w3-container w3-sand">
+    <?php 
+        include "connectDatabase.php";
+
+        $sql = "SELECT o.order_id, o.orderDate, c.lastName, c.firstName, b.title, bo.quantity, b.price, o.shipCost ";
+        $sql .= "FROM orders o ";
+        $sql .= "JOIN customer c ON o.customer_id = c.customer_id 
+                    JOIN book_order bo ON o.order_id = bo.order_id
+                    JOIN book b ON bo.book_id = b.book_id";
+
+        $result = $conn->query($sql);
+
+        if($result->num_rows > 0){
+            echo "<table class ='w3-table w3-striped'>";
+            echo "  <tr class='w3-teal'>";
+            echo "      <th>ID</th>";
+            echo "      <th>Date</th>";
+            echo "      <th>LastName</th>";
+            echo "      <th>First Name</th>";
+            echo "      <th>Book Title</th>";
+            echo "      <th>Qty</th>";
+            echo "      <th>$/Unit</th>";
+            echo "      <th>Shipping Cost</th>";
+            echo "  </tr>";
+
+            while($row = $result->fetch_assoc()) {
+            echo "  <tr>";
+            echo "      <td>".$row['order_id']."</td>";
+            echo "      <td>".$row['orderDate']."</td>";
+            echo "      <td>".$row['lastName']."</td>";
+            echo "      <td>".$row['firstName']."</td>";
+            echo "      <td>".$row['title']."</td>";
+            echo "      <td>".$row['quantity']."</td>";
+            echo "      <td>".$row['price']."</td>";
+            echo "      <td>".$row['shipCost']."</td>";
+            echo "  </tr>";
+            }
+
+            echo "</table>";
+        }
+        else {
+            echo "0 results<br>";
+        }
+
+        $conn->close();
+
+    ?>
+        </div>
+    </div>
+</body>
+</html>
