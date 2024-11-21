68e4ed90921a556c6236702a0c353b0371f8c15c
Shianne <slesure1@ggc.edu>
2024-11-21 11:35:01 -0500

    API is almost working

--- Projects/API/WeatherAPI.php
+++ Projects/API/WeatherAPI.php
@@ -8,26 +8,26 @@
     <link rel="stylesheet" href="https://www.w3schools.com/lib/w3-theme-indigo.css"> 
 </head>
 <body class="w3-theme-l5">
-    <header class="w3-display-container w3-center w3-thme-l3">
+    <header class="w3-display-container w3-center w3-theme-l3">
         <h1>Weather Info<h1>
     </header>
 
     <form action="WeatherAPI.php" class="w3-container w3-theme-l3" method="POST">
-        <label>City</label>
+        <label>City</label><br>
         <select name="selectedCity" class="w3-select">
-            <option value="" disabled selected>Choose Ciy</option>
+            <option value="" disabled selected>Choose City</option>
             <?php 
             include "connectDatabase.php";
 
             //retrieve cities
-            $sql = "SELECT ct.name as countryName, cy.name as cityname, cy.city_id";
-            $sql .= "FROM country ct, city cy";
-            $sql .= "WHERE ct.country_id = cy.country_id";
+            $sql = "SELECT ct.name as countryName, cy.name as cityName, cy.city_id ";
+            $sql .= "FROM country ct, city cy ";
+            $sql .= "WHERE ct.country_id = cy.country_id ";
             $sql .= "ORDER BY countryName, cityName";
 
             $result = $conn->query($sql);
 
-            if($result->num_row > 0){
+            if($result->num_rows > 0){
                 while($row = $result->fetch_assoc()){
                     $cityId = $row['city_id'];
                     $country = $row['countryName'];
@@ -39,13 +39,14 @@
             $conn->close();
             ?>
         </select>
-        <br><input type="submit" name="submit" class="w3-btn w3-blue-grey" value="Get weather">
+        <p><input type="submit" name="submit" class="w3-btn w3-blue-grey" value="Get weather"></p>
     </form>
-    <div class="w3-container w3-themel4">
+    <div class="w3-container w3-theme-l4">
         <?php 
         if(isset($_POST['submit'])){
             if(!isset($_POST['selectedCity'])){
-                echo "Please select a city<br>";
+                echo "<p>You have not slected a city.<br>
+                Please go back and try again.</p>";
                 exit;
             }
 
@@ -53,55 +54,72 @@
 
             
 
-$curl = curl_init();
+        $curl = curl_init();
 
-curl_setopt_array($curl, [
-	CURLOPT_URL => "https://weatherapi-com.p.rapidapi.com/current.json?q=.$selectedCity",
-	CURLOPT_RETURNTRANSFER => true,
-	CURLOPT_ENCODING => "",
-	CURLOPT_MAXREDIRS => 10,
-	CURLOPT_TIMEOUT => 30,
-	CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-	CURLOPT_CUSTOMREQUEST => "GET",
-	CURLOPT_HTTPHEADER => [
-		"x-rapidapi-host: weatherapi-com.p.rapidapi.com",
-		"x-rapidapi-key: 0576dcb465msh443527aaf349783p182dc4jsn73089664fac7"
-	],
-]);
+        curl_setopt_array($curl, [
+	        CURLOPT_URL => "https://weatherapi-com.p.rapidapi.com/current.json?q=" . $selectedCity,
+	        CURLOPT_RETURNTRANSFER => true,
+	        CURLOPT_ENCODING => "",
+	        CURLOPT_MAXREDIRS => 10,
+	        CURLOPT_TIMEOUT => 30,
+	        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+	        CURLOPT_CUSTOMREQUEST => "GET",
+	        CURLOPT_HTTPHEADER => [
+		        "x-rapidapi-host: weatherapi-com.p.rapidapi.com",
+		        "x-rapidapi-key: 0576dcb465msh443527aaf349783p182dc4jsn73089664fac7"
+	        ],
+        ]);
 
-$response = curl_exec($curl);
-$err = curl_error($curl);
+        $data = curl_exec($curl);
+        $err = curl_error($curl);
 
-curl_close($curl);
+        curl_close($curl);
 
-if ($err) {
-	echo "cURL Error #:" . $err;
-} else {
-	echo $data;
+        if ($err) {
+	        echo "cURL Error #:" . $err;
+        } 
+        else {
+	        //echo $data;
 
-    $jsonData = $data; // store the original string data 
-    $data = json_decode($jsonData, true); // decode JSON string into assoc array
+            $jsonData = $data; // store the original string data 
+            $data = json_decode($jsonData, true); // decode JSON string into assoc array
     
-    echo "<br>";
-    if(json_last_error() === JSON_ERROR_NONE) {
-        // successfully decoded JSON
+            //print_r($data);
+            echo "<br>";
+            if(json_last_error() === JSON_ERROR_NONE) {
+                 // successfully decoded JSON
 
-        $currentCondition = $data['current']['condition']['text'];
-        $icon = data['current']['condition']['icon'];
+                $currentCondition = $data['current']['condition']['text'];
+                 $icon = $data['current']['condition']['icon'];
 
-        <div class="w3-third">
+                ?>
+            <div class="w3-third">
             <div class="w3-card">
-                echo "<b>Country:"
+                <?php
+                echo "<b>Country</b>:" . $data['location']['country'] . "<br>";
+                echo "<b>Region</b>: " . $data['location']['region'] . "<br>";
+                echo "<b>Local Time</b>:" . $data['location']['localtime'] . "<br>";
+                echo "<b>Temperature Celsius</b>: " . $data['location']['temp_c'] . "<br>";
+                echo "<b>Temprature Fahrenheit</b>:" . $data['location']['temp_f'] . "<br>";
+                ?>
+                <img src="<?php echo $icon;?>">
+                <div class="w3-container">
+                <h4><?php echo $currentCondition; ?></h4>
+                </div>
             </div>
         </div>
+        <?php
     }
     else {
+        // handle JSON decoding error
         echo "Error: failed to be decode JSON data";
     }
+    }
 }
-        }
         ?>
     </div>
+</div>
+    
     
 </body>
 </html>
--- Projects/API/connectDatabase.php
+++ Projects/API/connectDatabase.php
@@ -4,10 +4,10 @@ $username = "guest";
 $password = "ggcITEC4450@";
 $dbname = "countries";
 
-$conn - new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 if($conn->connect_error){
-    die("Connection failed: ".$conn->connect_error);
+    die("Connection failed: ". $conn->connect_error);
 }
 
 ?>
